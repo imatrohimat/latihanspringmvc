af8fe97b0fd95ac5034be566d99efb9e39290db4
imatrohimat <imat.neutron@gmail.com>
2019-07-04 17:58:53 +0700

    first commit

---
+++ src/main/java/com/eksad/latihanspringmvc/controller/Hellocontroller.java
@@ -0,0 +1,35 @@
+package com.eksad.latihanspringmvc.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class Hellocontroller {
+
+	@RequestMapping("/hello")
+	@ResponseBody
+	public String sayhello () {
+		return "hello world";
+	}
+		
+		@RequestMapping("/myName")
+		@ResponseBody
+		public String myName(@RequestParam String myName) {
+			return "hello   "+ myName;
+	}
+		
+		@RequestMapping("/namaSaya/{myName}")
+		@ResponseBody
+		public String myName2(@PathVariable String myName) {
+			return "hello   "+ myName;}
+		
+		@RequestMapping("/toGoogle")
+		@ResponseBody
+		public String toGoogle() {
+			return "<a href='http://www.google.com'> Klik disini menuju Google </a>"
+					+ "</br> <a href='https://www.youtube.com'>klik di sini menuju Youtube</a>";
+		}
+}
---
+++ src/main/java/com/eksad/latihanspringmvc/repository/ProductRevositoryDAO.java
@@ -0,0 +1,11 @@
+package com.eksad.latihanspringmvc.repository;
+
+
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.eksad.latihanspringmvc.model.Product;
+
+public interface ProductRevositoryDAO extends JpaRepository<Product, Long>{
+
+}

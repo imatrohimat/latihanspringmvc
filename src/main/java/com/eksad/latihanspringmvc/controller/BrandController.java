169c2b1b695fdeced13077b2982c4a330d63fbab
imatrohimat <imat.neutron@gmail.com>
2019-07-09 22:55:29 +0700

    update

--- src/main/java/com/eksad/latihanspringmvc/controller/BrandController.java
+++ src/main/java/com/eksad/latihanspringmvc/controller/BrandController.java
@@ -1,10 +1,6 @@
 package com.eksad.latihanspringmvc.controller;
 
-import java.lang.reflect.Member;
 import java.util.List;
-
-import javax.servlet.http.HttpServletRequest;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -12,7 +8,6 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-
 import com.eksad.latihanspringmvc.model.Brand;
 import com.eksad.latihanspringmvc.repository.BrandRevositoryDAO;
 
@@ -20,18 +15,15 @@ import com.eksad.latihanspringmvc.repository.BrandRevositoryDAO;
 @RequestMapping("brand")
 public class BrandController {
 
-private BrandController brandcontroller;
-
 	@Autowired
 	BrandRevositoryDAO brandRevositoryDAO;
-	
+
 	@RequestMapping("")
 	public String index(Model model) {
 		List<Brand> listBrand = brandRevositoryDAO.findAll();
 		model.addAttribute("listBrand", listBrand);
 		return "listBrand";
 	}
-	
 
 	@RequestMapping("/addBrand")
 	public String addBrand(Model model) {
@@ -41,42 +33,26 @@ private BrandController brandcontroller;
 		model.addAttribute("brand", brand);
 		return "addBrand";
 	}
-	
+
 	@RequestMapping(value = "/save", method = RequestMethod.POST)
 	public String save(@ModelAttribute Brand brand) {
 		brandRevositoryDAO.save(brand);
 
 		return "redirect:/brand";
 	}
-	 
-	@RequestMapping(value = "/editBrand{id}", method = RequestMethod.GET)
-	public String editBrand(Model model, @PathVariable int id) {
-	   String idAsString = Integer.toString(id);
-	    model.addAttribute("brand",brandcontroller.getBrandById(idAsString));
-	    return "editBrand{id}";
-}
-	private Object getBrandById(String idAsString) {
-		// TODO Auto-generated method stub
-		return null;
-	}
 
-	@RequestMapping("/deleteBrand")
-	public String delete(Model model) {
-
-		Brand brand = new Brand();
+	@RequestMapping(value = "/edit/{id}", method = RequestMethod.GET)
+	public String editBrand(@PathVariable Long id, Model model) {
+		Brand brand = brandRevositoryDAO.findOne(id);
 		model.addAttribute("brand", brand);
-		return "deleteBrand";
-	
-}
-}
-	
-	/* CARA LAIN
-	@RequestMapping(value = "editBrand{id}", method = RequestMethod.POST)
-	public String updateBrand(@PathVariable int id, 
-	        HttpServletRequest request, Brand brand) {
-		 brandcontroller.updateBrand(id, request, brand);
-		    return "editBrand";
-		}
+
+		return "addBrand";
 	}
-	
-	*/
+
+	@RequestMapping("/delete/{id}")
+	public String delete(@PathVariable Long id) {
+		brandRevositoryDAO.delete(id);
+		return "redirect:/brand";
+
+	}
+}
